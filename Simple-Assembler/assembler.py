a3170a9963c317ab301b0212871c8ee2631b2698
Pranay Gupta <pranaygupta2512@gmail.com>
2021-08-10 18:34:26 +0530

    basic error checking
    added basic error checks for instruction syntax and halt being the last instruction | added the dictionaries for labels and variables

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -8,15 +8,92 @@ for line in sys.stdin:
 convertedBinary = []                # append the result statements to be printed in this list
 encounteredErrors = []              # append any encountered errors to this list
 
+emptyLines = 0
+for line in assemblyCode:
+    if(not line):
+        emptyLines += 1
+
+haltEncountered = False
+labels = dict()
+variables = dict()
+nextVariableLocation = len(assemblyCode)-emptyLines
+
+for lineNumber in range(len(assemblyCode)):
+    if(not assemblyCode[lineNumber]):
+        continue
+    else:
+        currentLine = assemblyCode[lineNumber].split()
+        if(currentLine[0][-1]==":"):
+            labels[currentLine[0][:len(currentLine[0]):]] = binary8bit(lineNumber)
+            currentLine.pop(0)
+        elif(currentLine[0]=="var"):
+            currentLine.pop(0)
+            if(len(currentLine)==1):
+                variables[currentLine[0]] = binary8bit(nextVariableLocation)
+                nextVariableLocation += 1
+            else:
+                encounteredErrors.append("ERROR at Line "+str(lineNumber+1)+": Improper variable declaration")
 
 
+        # -------------------------------------------------------------------------------------------------------
+        if(haltEncountered):
+            encounteredErrors.append("ERROR at Line "+str(lineNumber+1)+": Halt(hlt) not being used as the last instruction")
+            continue
+        # -------------------------------------------------------------------------------------------------------
 
 
+        # -------------------------------------------------------------------------------------------------------
+        if(typeOfInstruction(currentLine[0])==-1):
+            encounteredErrors.append("ERROR at Line "+str(lineNumber+1)+": Invalid instruction")
+            continue
+        # -------------------------------------------------------------------------------------------------------
 
 
+        # -------------------------------------------------------------------------------------------------------
+        if(typeOfInstruction(currentLine[0])=='a' and len(currentLine)==4):
+            pass
+        else:
+            encounteredErrors.append("ERROR at Line "+str(lineNumber+1)+": Wrong Syntax used for instruction")
+        # -------------------------------------------------------------------------------------------------------
+        
 
+        # -------------------------------------------------------------------------------------------------------
+        if(typeOfInstruction(currentLine[0])=='b' and len(currentLine)==3):
+            pass
+        else:
+            encounteredErrors.append("ERROR at Line "+str(lineNumber+1)+": Wrong Syntax used for instruction")
+        # -------------------------------------------------------------------------------------------------------
+        
 
+        # -------------------------------------------------------------------------------------------------------
+        if(typeOfInstruction(currentLine[0])=='c' and len(currentLine)==3):
+            pass
+        else:
+            encounteredErrors.append("ERROR at Line "+str(lineNumber+1)+": Wrong Syntax used for instruction")
+        # -------------------------------------------------------------------------------------------------------
+        
 
+        # -------------------------------------------------------------------------------------------------------
+        if(typeOfInstruction(currentLine[0])=='d' and len(currentLine)==3):
+            pass
+        else:
+            encounteredErrors.append("ERROR at Line "+str(lineNumber+1)+": Wrong Syntax used for instruction")
+        # -------------------------------------------------------------------------------------------------------
+        
+
+        # -------------------------------------------------------------------------------------------------------
+        if(typeOfInstruction(currentLine[0])=='e' and len(currentLine)==2):
+            pass
+        else:
+            encounteredErrors.append("ERROR at Line "+str(lineNumber+1)+": Wrong Syntax used for instruction")
+        # -------------------------------------------------------------------------------------------------------
+        
+
+        # -------------------------------------------------------------------------------------------------------
+        if(typeOfInstruction(currentLine[0])=='f' and len(currentLine)==1):
+            convertedBinary.append(opcode(currentLine[0])+'0'*11)
+            haltEncountered = True
+        # -------------------------------------------------------------------------------------------------------
 
 
 
