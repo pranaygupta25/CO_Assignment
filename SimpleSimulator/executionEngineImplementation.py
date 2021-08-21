6132ceaef7aeb096de8b916b02bf7080750a82d2
NakulPeriwal <nakul20316@iiitd.ac.in>
2021-08-21 23:16:20 +0530

    XOR implemented

--- SimpleSimulator/executionEngineImplementation.py
+++ SimpleSimulator/executionEngineImplementation.py
@@ -149,7 +149,9 @@ class ExecutionEngine:
             reg1 = instruction[ 7:10:]      # reading address of reg1
             reg2 = instruction[10:13:]      # reading address of reg2
             reg3 = instruction[13:16:]      # reading address of reg3
-            pass
+
+            res = RF.getRegister(reg2,False) ^ RF.getRegister(reg3,False)
+            RF.setRegister(reg1,res)
             RF.resetFlagRegister()
             (halt, newPC) = (False, PC.getValue() + 1)
         # ........................................................................................................................
